e1b41d775395877a4815946a4c0943025ee28719
Symoviez1 <sachinya1990@gmail.com>
2025-08-05 02:51:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -81,7 +81,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('🤖 ʙᴏᴛ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/Symoviez')
             ],[
-            InlineKeyboardButton('🔍 Movie Request ɢroup', url='https://t.me/+vmAQ2CCdND5iMTBl'),
+            InlineKeyboardButton('🔍 Movie Request ɢroup', url='https://t.me/Symoviez1'),
             InlineKeyboardButton('🍿 Main ᴄʜᴀɴɴᴇʟ', url='https://t.me/Symoviez')
             ],[
             InlineKeyboardButton('🎁 ʜᴇʟᴘ', callback_data='help'),
